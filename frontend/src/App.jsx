580b0d1ac3b62c52e630bda81cfb37e7a3bb08d5
sahritika07 <ritikasah731@gmail.com>
2025-11-11 06:52:31 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Tablelist from './components/Tablelist'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Tablelist/>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Tablelist.jsx
@@ -0,0 +1,30 @@
+export default function Tablelist() {
+    clients = [{ }]
+    return (
+        <>
+            <div className="overflow-x-auto mt-10">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                        </tr>
+                    </thead>
+                    <tbody className="hover:bg-base-300">
+                        {/* row 1 */}
+                        <tr>
+                            <th>1</th>
+                            <td>Cy Ganderton</td>
+                            <td>Quality Control Specialist</td>
+                            <td>Blue</td>
+                        </tr>
+  
+                    </tbody>
+                </table>
+            </div>
+        </>
+    )
+}
