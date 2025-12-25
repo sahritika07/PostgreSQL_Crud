422ebcf8f0b2487967786908b2491ffb0294310a
sahritika07 <ritikasah731@gmail.com>
2025-12-25 21:06:50 +0530

    first commit

---
+++ src/controllers/userController.js
@@ -0,0 +1,57 @@
+import pool from "../config/db.js";
+
+// CREATE
+export const createUser = async (req, res) => {
+  const { name, email, age } = req.body;
+
+  try {
+    const result = await pool.query(
+      "INSERT INTO users (name, email, age) VALUES ($1, $2, $3) RETURNING *",
+      [name, email, age]
+    );
+
+    res.status(201).json({
+      success: true,
+      data: result.rows[0],
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+
+
+
+
+// READ
+export const getUsers = async (req, res) => {
+  const result = await pool.query("SELECT * FROM users ORDER BY id DESC");
+  res.json({ success: true, data: result.rows });
+};
+
+
+
+
+// UPDATE
+export const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const { name, age } = req.body;
+
+  const result = await pool.query(
+    "UPDATE users SET name=$1, age=$2 WHERE id=$3 RETURNING *",
+    [name, age, id]
+  );
+
+  res.json({ success: true, data: result.rows[0] });
+};
+
+
+
+
+// DELETE
+export const deleteUser = async (req, res) => {
+  const { id } = req.params;
+
+  await pool.query("DELETE FROM users WHERE id=$1", [id]);
+  res.json({ success: true, message: "User deleted" });
+};
---
+++ src/routes/userRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  createUser,
+  getUsers,
+  updateUser,
+  deleteUser,
+} from "../controllers/userController.js";
+
+const router = express.Router();
+
+router.post("/", createUser);
+router.get("/", getUsers);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
+
+export default router;
